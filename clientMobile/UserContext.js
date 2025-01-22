78aba6f339b0f7c2a2e25af30506247c8b0b5a95
Kaïs Dilmi <146418888+Iska1108@users.noreply.github.com>
2025-01-22 22:13:13 +0100

    QR-code + Settings

--- clientMobile/UserContext.js
+++ clientMobile/UserContext.js
@@ -1,10 +1,19 @@
-// UserContext.js
 import React, { createContext, useState } from "react";
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState({
+    ID_Client: null,
+    name: "",
+    surname: "",
+    mail: "",
+    num: "",
+    contact_num: "",
+    birth: "",
+    handicap: "",
+    contact_mail: "",
+  });
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
